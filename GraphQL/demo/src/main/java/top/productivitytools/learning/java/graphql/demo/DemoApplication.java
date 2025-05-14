ff8c62b3caa87152559f25f7723bf9adf02b5d3a
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-14 22:56:29 +0200

    service with corrct references

--- GraphQL/demo/src/main/java/top/productivitytools/learning/java/graphql/demo/DemoApplication.java
+++ GraphQL/demo/src/main/java/top/productivitytools/learning/java/graphql/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package top.productivitytools.learning.GraphQl.pwdemoapplication;
+package top.productivitytools.learning.java.graphql.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PwdemoapplicationApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PwdemoapplicationApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- GraphQL/demo/src/test/java/top/productivitytools/learning/java/graphql/demo/DemoApplicationTests.java
+++ GraphQL/demo/src/test/java/top/productivitytools/learning/java/graphql/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package top.productivitytools.learning.GraphQl.pwdemoapplication;
+package top.productivitytools.learning.java.graphql.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PwdemoapplicationApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
